54939ea2c06cda8bd67b4b88ab94a6b54d556cf8
ArdaCimen <cimenarda.yaz@gmail.com>
2024-04-01 15:52:49 +0300

    Şifre Değiştirme Sayfası Oluşturulup Kodlandı

--- AspNetCoreIdentity.Web/Controllers/MemberController.cs
+++ AspNetCoreIdentity.Web/Controllers/MemberController.cs
@@ -1,4 +1,5 @@
-﻿using AspNetCoreIdentity.Web.Models;
+﻿using AspNetCoreIdentity.Web.Extentions;
+using AspNetCoreIdentity.Web.Models;
 using AspNetCoreIdentity.Web.ViewModels;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
@@ -27,6 +28,53 @@ namespace AspNetCoreIdentity.Web.Controllers
 
             return View(userviewModel);
         }
+
+
+
+
+
+        public IActionResult PasswordChange()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> PasswordChange(PasswordChangeViewModel request)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View();
+            }
+
+
+            var currentUser = await _userManager.FindByNameAsync(User.Identity!.Name!);
+
+            var checkOldPassword = await _userManager.CheckPasswordAsync(currentUser,request.PasswordOld);
+            
+            if (!checkOldPassword)
+            {
+                ModelState.AddModelError(string.Empty, "Eski şifreniz yanlış");
+                return View();
+            }
+            var resultChangePassword = await _userManager.ChangePasswordAsync(currentUser,request.PasswordOld, request.PasswordNewConfirm!);
+
+            if (!resultChangePassword.Succeeded)
+            {
+                ModelState.AddErrorModelList(resultChangePassword.Errors.Select(x=>x.Description).ToList());
+                return View();
+            }
+
+            await _userManager.UpdateSecurityStampAsync(currentUser);
+            await _signInManager.SignOutAsync();
+            await _signInManager.PasswordSignInAsync(currentUser,request.PasswordNew!,true,false);
+
+
+            TempData["SuccessMessage"] = "Şifreniz başarıyla değiştirilmiştir";
+
+            return View();
+        }
+
+
 
 
         public async Task<IActionResult> Logout()
---
+++ AspNetCoreIdentity.Web/ViewModels/PasswordChangeViewModel.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+
+namespace AspNetCoreIdentity.Web.ViewModels
+{
+    public class PasswordChangeViewModel
+    {
+        [DataType(DataType.Password)]
+        [DisplayName("Eski Şifre")]
+        [Required(ErrorMessage = "Eski Şifre alanı boş bırakılamaz.")]
+        [MinLength(6,ErrorMessage ="Şifreniz en az 6 karakter olabilir.")]
+        public string? PasswordOld { get; set; }
+
+
+        [DataType(DataType.Password)]
+        [DisplayName("Yeni Şifre")]
+        [Required(ErrorMessage = " Yeni Şifre alanı boş bırakılamaz.")]
+        [MinLength(6, ErrorMessage = "Şifreniz en az 6 karakter olabilir.")]
+        public string? PasswordNew { get; set; }
+
+        [DataType(DataType.Password)]
+        [DisplayName("Yeni Şifre Tekrar")]
+        [Compare(nameof(PasswordNew), ErrorMessage = "Şifreler uyuşmuyor.")]
+        [Required(ErrorMessage = " Yeni Şifre tekrar alanı boş bırakılamaz.")]
+        [MinLength(6, ErrorMessage = "Şifreniz en az 6 karakter olabilir.")]
+        public string? PasswordNewConfirm { get; set; }
+
+    }
+}
