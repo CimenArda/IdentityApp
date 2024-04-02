34362f4d77abe6a15f64d76e4e9fe0de2bab18a0
ArdaCimen <cimenarda.yaz@gmail.com>
2024-04-02 15:01:26 +0300

    Rol Bazlı Yetkilendirme İşlemleri-1

---
+++ AspNetCoreIdentity.Web/Areas/Admin/Controllers/RoleController.cs
@@ -0,0 +1,24 @@
+﻿using AspNetCoreIdentity.Web.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AspNetCoreIdentity.Web.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class RoleController : Controller
+    {
+        private readonly UserManager<AppUser> _userManager;
+        private readonly RoleManager<AppRole> _roleManager;
+
+        public RoleController(UserManager<AppUser> userManager, RoleManager<AppRole> roleManager)
+        {
+            _userManager = userManager;
+            _roleManager = roleManager;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
