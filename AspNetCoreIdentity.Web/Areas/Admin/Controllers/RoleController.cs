a4326e66388dd0f38c458b33d00b50209930e909
ArdaCimen <cimenarda.yaz@gmail.com>
2024-04-03 15:57:14 +0300

    Rol Atama İşlemleri yapıldı

--- AspNetCoreIdentity.Web/Areas/Admin/Controllers/RoleController.cs
+++ AspNetCoreIdentity.Web/Areas/Admin/Controllers/RoleController.cs
@@ -3,6 +3,7 @@ using AspNetCoreIdentity.Web.Extentions;
 using AspNetCoreIdentity.Web.Models;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace AspNetCoreIdentity.Web.Areas.Admin.Controllers
 {
@@ -49,7 +50,6 @@ namespace AspNetCoreIdentity.Web.Areas.Admin.Controllers
 
             return RedirectToAction("Index");
         }
-
 
 
 
@@ -102,5 +102,63 @@ namespace AspNetCoreIdentity.Web.Areas.Admin.Controllers
 
             return RedirectToAction("Index");
         }
+
+
+
+        public async Task<IActionResult> AssignRoleToUser(string id)
+        {
+            var currentUser = await _userManager.FindByIdAsync(id);
+            ViewBag.userId = id;
+            var roles = await _roleManager.Roles.ToListAsync();
+
+            var userRoles = await _userManager.GetRolesAsync(currentUser);
+
+            var roleViewModelList = new List<AssignRoleToUserViewModel>();
+
+            foreach (var role in roles)
+            {
+                var assignRoleToUserViewModel = new AssignRoleToUserViewModel()
+                {
+                    Id = role.Id,
+                    Name = role.Name
+                };
+
+                if (userRoles.Contains(role.Name!))
+                {
+                    assignRoleToUserViewModel.Exist = true;
+                }
+
+                roleViewModelList.Add(assignRoleToUserViewModel);
+
+            }
+
+            return View(roleViewModelList);
+        }
+
+
+        [HttpPost]
+        public async Task<IActionResult> AssignRoleToUser(string userId,List<AssignRoleToUserViewModel> requestList)
+        {
+
+            var userToAssignRoles =await _userManager.FindByIdAsync(userId);
+
+            foreach (var role in requestList)
+            {
+                if (role.Exist)
+                {
+                    await _userManager.AddToRoleAsync(userToAssignRoles,role.Name);
+
+                }
+                else
+                {
+                    await _userManager.RemoveFromRoleAsync(userToAssignRoles,role.Name);
+                }
+            }
+
+
+            return RedirectToAction("UserList", "Home");
+        }
+
+
     }
 }
